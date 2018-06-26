28bd62bf2ce91eb33e5623dfcca98ebfb82577ff
amirhossein <s.amir.s1372@gmail.com>
2018-06-27 00:33:26 +0430

    d

--- server.js
+++ server.js
@@ -1,25 +1,140 @@
-'use strict';
+'use strict'
 
-const express = require('express');
-const SocketServer = require('ws').Server;
-const path = require('path');
+const express = require('express')
+const SocketServer = require('ws').Server
+const path = require('path')
 
-const PORT = process.env.PORT || 3000;
-const INDEX = path.join(__dirname, 'index.html');
+const PORT = process.env.PORT || 3000
+const INDEX = path.join(__dirname, 'index.html')
 
 const server = express()
   .use((req, res) => res.sendFile(INDEX) )
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
-const wss = new SocketServer({ server });
+const wss = new SocketServer({ server })
 
-wss.on('connection', (ws) => {
-  console.log('Client connected');
-  ws.on('close', () => console.log('Client disconnected'));
-});
+// wss.on('connection', (ws) => {
+//   console.log('Client connected')
+//   ws.on('close', () => console.log('Client disconnected'))
+// })
 
-setInterval(() => {
-  wss.clients.forEach((client) => {
-    client.send(new Date().toTimeString());
-  });
-}, 1000);
+const users = {}
+
+wss.on('connection', function(connection) {
+
+   connection.on('message', function(message) {
+
+      let data
+
+      // accepting only JSON messages
+      try {
+         data = JSON.parse(message)
+      } catch (e) {
+         // invalid JSON
+         data = {}
+      }
+
+      switch (data.type) {
+         case 'login': {
+           users[data.name] = connection
+           connection.name = data.name
+
+           sendTo(connection, {
+             type: 'login',
+             success: true
+           })
+
+           break
+         }
+
+         case 'offer': {
+           const conn = users[data.name]
+
+           if(conn != null) {
+              connection.otherName = data.name
+
+              sendTo(conn, {
+                 type: 'offer',
+                 offer: data.offer,
+                 name: connection.name
+              })
+           }
+
+           break
+         }
+
+         case 'answer': {
+           const conn = users[data.name]
+
+           if(conn != null) {
+              connection.otherName = data.name
+              sendTo(conn, {
+                 type: 'answer',
+                 answer: data.answer
+              })
+           }
+
+           break
+         }
+
+         case 'candidate': {
+           const conn = users[data.name]
+
+           if(conn != null) {
+              sendTo(conn, {
+                 type: 'candidate',
+                 candidate: data.candidate
+              })
+           }
+
+           break
+         }
+
+         case 'leave': {
+           const conn = users[data.name]
+           conn.otherName = null
+
+           if(conn != null) {
+              sendTo(conn, {
+                 type: 'leave'
+              })
+           }
+
+           break
+         }
+
+         default:
+            sendTo(connection, {
+               type: 'error',
+               message: 'Command not found: ' + data.type
+            })
+
+            break
+      }
+   })
+
+   // in case of closing browser tab
+   connection.on('close', function() {
+
+      if(connection.name) {
+         delete users[connection.name]
+
+         if(connection.otherName) {
+
+            const conn = users[connection.otherName]
+            conn.otherName = null
+
+            if(conn != null) {
+               sendTo(conn, {
+                  type: 'leave'
+              })
+            }
+         }
+      }
+   })
+
+})
+
+function sendTo(connection, message) {
+   connection.send(JSON.stringify(message))
+}
