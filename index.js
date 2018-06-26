40bf74d83c8133b9ec79bb9f0cb4fd7cec4827fa
amirhossein <s.amir.s1372@gmail.com>
2018-06-26 22:51:34 +0430

    lls

--- index.js
+++ index.js
@@ -142,7 +142,7 @@ var express = require("express")
      res.sendfile( __dirname + req.params[0])
  })
 
- var port = process.env.PORT || 5000
+ var port = process.env.PORT || 8080
  app.listen(port, function() {
    console.log("Listening on " + port)
  })
