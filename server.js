4fc126cc418196d9be7884b3d728411d216e4c04
amirhossein <s.amir.s1372@gmail.com>
2018-07-16 15:49:32 +0430

    123

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ wss.on('connection', function(connection) {
 
       switch (data.type) {
          case 'login': {
-           console.log(log)
+           console.log(data)
            users[data.wisId][data.name] = connection
            connection.name = data.name
 
